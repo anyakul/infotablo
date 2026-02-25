07109706e5d89a67a31c25cbcd6aeb9248a23a97
anyakul <kulikow88@inbox.ru>
2026-02-26 02:45:17 +0300

    Делаю сайт

--- src/js/modules/fullscreen.js
+++ src/js/modules/fullscreen.js
@@ -1,31 +1,3 @@
-/*export const fullscreen = () => {
-  function toggleFullscreen() {
-    const element = document.querySelector('#player');
-
-    if (!document.fullscreenElement) {
-      element.requestFullscreen().catch(err => {
-        console.error(`Ошибка: ${err.message}`);
-      });
-    } else {
-      document.exitFullscreen();
-    }
-  }
-
-  const playerFullscreen = document.querySelector('#player-fullscreen');
-
-  if (playerFullscreen) {
-    playerFullscreen.addEventListener('click', toggleFullscreen);
-  }
-
-  document.addEventListener('fullscreenchange', function() {
-    if (!document.fullscreenElement) {
-      document.body.classList.remove('fullscreen-mode');
-    } else {
-      document.body.classList.add('fullscreen-mode');
-    }
-  });
-}*/
-
 export const fullscreen = () => {
   function toggleFullscreen() {
     const element = document.querySelector('#player');
@@ -39,13 +11,27 @@ export const fullscreen = () => {
     }
   }
 
-  const playerFullscreen = document.querySelector('#player-fullscreen');
+  const playerFullscreen = document.querySelector('#player');
+
+  const playerFullscreenBtn = document.querySelector('#player-fullscreen');
 
   if (playerFullscreen) {
     playerFullscreen.addEventListener('click', toggleFullscreen);
   }
 
-  document.addEventListener('fullscreenchange', function() {
+  playerFullscreen.addEventListener('click', () => {
+    if (!document.fullscreenElement) {
+      document.body.classList.remove('fullscreen-mode');
+    } else {
+      document.body.classList.add('fullscreen-mode');
+    }
+  });
+
+  if (playerFullscreenBtn) {
+    playerFullscreen.addEventListener('click', toggleFullscreen);
+  }
+
+  playerFullscreen.addEventListener('click', () => {
     if (!document.fullscreenElement) {
       document.body.classList.remove('fullscreen-mode');
     } else {
